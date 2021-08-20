ae4be0f477e222fc1da26383f4da6f2a593add4c
mmoo55 <g.gerson51@gmail.com>
2021-08-20 16:18:43 -0400

    Calcular moda convertido a función

--- moda.js
+++ moda.js
@@ -1,32 +1,56 @@
-const lista1 = [
-    1,
-    2,
-    3,
-    1,
-    2,
-    3,
-    4,
-    2,
-    2,
-    2,
-    1
-];
+// const lista1 = [
+//     1,
+//     2,
+//     3,
+//     1,
+//     2,
+//     3,
+//     4,
+//     2,
+//     2,
+//     2,
+//     1
+// ];
 
-const lista1Count = {};
+// const lista1Count = {};
 
-lista1.map(
-    function (elemento) {
-        if (lista1Count[elemento]) {
-            lista1Count[elemento] += 1;
-        } else {
-            lista1Count[elemento] = 1;
+// lista1.map(
+//     function (elemento) {
+//         if (lista1Count[elemento]) {
+//             lista1Count[elemento] += 1;
+//         } else {
+//             lista1Count[elemento] = 1;
+//         }
+//     }
+// );
+
+// const lista1Array = Object.entries(lista1Count).sort(   // Object.entries Convierte un objeto en un array
+//     function (valorAcumulado, nuevoValor) {
+//         return valorAcumulado[1] - nuevoValor[1];
+//     });
+
+// const moda = lista1Array[lista1Array.length - 1];
+
+
+function calcularModa (lista) {
+    let listaCount = {};
+
+    lista.map(
+        function (elemento) {
+            if (listaCount[elemento]) {
+                listaCount[elemento] += 1;
+            } else {
+                listaCount[elemento] = 1;
+            }
         }
-    }
-);
+    );
 
-const lista1Array = Object.entries(lista1Count).sort(   // Object.entries Convierte un objeto en un array
-    function (valorAcumulado, nuevoValor) {
-        return valorAcumulado[1] - nuevoValor[1];
-    });
+    const listaArray = Object.entries(listaCount).sort(
+        function (valorAcumulado, nuevoValor) {
+            return valorAcumulado[1] - nuevoValor[1];
+        });
+    
+    const moda = listaArray[listaArray.length - 1];
 
-const moda = lista1Array[lista1Array.length - 1];
+    return moda;
+}
