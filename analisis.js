72c0600d120146f202fa255561a798c3c297b2e8
mmoo55 <g.gerson51@gmail.com>
2021-08-21 21:17:07 -0400

    Claculando el top 10 de la media de salarios

--- analisis.js
+++ analisis.js
@@ -1,15 +1,4 @@
-const salariosCol = colombia.map(
-    function (personita) {
-        return personita.salary;
-    }
-);
-
-const salariosColSorted = salariosCol.sort(
-    function (salaryA, salaryB) {
-        return salaryA - salaryB;
-    }
-);
-
+// Helpers
 function esPar(numerito) {
     return (numerito % 2 === 0);
 }
@@ -26,6 +15,7 @@ function calcularMediaAritmetica(lista) {
     return promedioLista;
 }
 
+// Calculadora de mediana
 function medianaSalarios(lista) {
     const mitad = parseInt(lista.length / 2);
 
@@ -41,5 +31,38 @@ function medianaSalarios(lista) {
     }
 }
 
+// Mediana General
+const salariosCol = colombia.map(
+    function (personita) {
+        return personita.salary;
+    }
+);
 
-console.log(medianaSalarios(salariosColSorted));
+const salariosColSorted = salariosCol.sort(
+    function (salaryA, salaryB) {
+        return salaryA - salaryB;
+    }
+);
+
+const medianaGeneralCol = medianaSalarios(salariosColSorted);
+
+// Mediana del top 10%
+
+// EJEMPLO splice
+// const arrayEjemplo = [0,1,2,3,4,5,||||6,7|||,8,9,10];
+// const EJEMPLO = arrayEjemplo.splice(5, 2);  // splice para extraer elementos de un array, argumentos: primero se envia la posici+on en la que queremos que comience; el segundo argumento es para la cantidad que extraeremos en el ejemplo quedaria [6,7]
+
+const spliceStart = (salariosColSorted.length * 90) / 100;
+const spliceCount = salariosColSorted.length - spliceStart;
+
+const salariosColTop10 = salariosColSorted.splice(
+    spliceStart, 
+    spliceCount
+);
+
+const medianaTop10Col = medianaSalarios(salariosColTop10);
+
+console.log({
+    medianaGeneralCol,
+    medianaTop10Col
+});
